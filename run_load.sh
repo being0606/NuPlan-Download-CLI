6e2db5a8b593f9a5dea2a8f4bca3df3fb621459d
being0606 <onuhjin@gmail.com>
2025-07-24 06:20:35 +0000

    Fix: Update run_load.sh to remove mini dataset option from download command

--- run_load.sh
+++ run_load.sh
@@ -3,6 +3,6 @@
 ID="template@template.com"
 PW="template_password"
 
-# Mini 데이터셋 다운로드 (개발/테스트용 - 가장 작은 크기)
+# Mini 데이터셋 다운로드
 python download_nuplan.py --username "$ID" --password "$PW" \
-    --mini-set --maps --log-db
+    --maps --log-db
